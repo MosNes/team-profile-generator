b725f60ff531d238f690c8f97392b270a2f43634
MosNes <7hekarl@gmail.com>
2022-07-03 11:36:10 -0400

    tested and added getId() method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -12,10 +12,17 @@ test('create an Employee Object with name, ID, and email address', () => {
     expect(employee.email).toEqual('bob@company.com');
 });
 
-//method
-
+//method tests
 test('gets employee name with a capital letter',() => {
     const employee = new Employee('bob','42','bob@company.com');
     console.log(employee.getName());
     expect(employee.getName()).toEqual('Bob');
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('gets employee ID',() => {
+    const employee = new Employee('bob','42','bob@company.com');
+    console.log(employee.getId());
+    expect(employee.getId()).toEqual('42');
+    expect(employee.getId()).toEqual(expect.any(String));
 });
