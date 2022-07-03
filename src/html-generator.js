6a173ca1b77b04c7190f7261b3a2fb7ca98a1e14
MosNes <7hekarl@gmail.com>
2022-07-03 18:31:23 -0400

    tested and added createHTML function

--- src/html-generator.js
+++ src/html-generator.js
@@ -83,6 +83,32 @@ const createCards = employeeArray => {
    return cardArr.join('');
 };
 
-console.log(createCards(employeeArray));
+const createHTML = employeeArray => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <script src="https://kit.fontawesome.com/3d68cbfa56.js" crossorigin="anonymous"></script>
+    <title>My Team</title>
+</head>
+<body>
+    <header class="has-background-primary p-6">
+        <h1 class="has-text-white is-size-1 has-text-centered has-text-weight-bold">My Team</h1>
+    </header>
+    <main class="p-6">
+        <div class="columns is-multiline is-4 is-centered">
+            ${createCards(employeeArray)}
+        </div>
+    </main>
+</body>
+</html>
+    `
+};
 
-module.exports = {createSpecialBox, createRoleElement, createEmployeeCard, createCards};
+console.log(createHTML(employeeArray));
+
+module.exports = {createHTML, createSpecialBox, createRoleElement, createEmployeeCard, createCards};
