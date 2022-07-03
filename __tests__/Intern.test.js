c8fe4bb3b3063be8d16cd54cd864bec2fa172ad4
MosNes <7hekarl@gmail.com>
2022-07-03 15:16:44 -0400

    tested and added getSchool and getRole methods to Intern class

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -9,5 +9,17 @@ test('create an Intern Object with name, ID, email address, and school', () => {
     expect(intern.name).toEqual('bob');
     expect(intern.id).toEqual('42');
     expect(intern.email).toEqual('bob@company.com');
-    expect(intern.school).toEqual('Bob University');
+    expect(intern.school).toEqual('bob university');
+});
+
+test('gets intern school with each word capitalized', () => {
+    const intern = new Intern('bob','42','bob@company.com','bob university');
+
+    expect(intern.getSchool()).toEqual('Bob University');
+});
+
+test('gets intern role', () => {
+    const intern = new Intern('bob','42','bob@company.com','bob university');
+
+    expect(intern.getRole()).toEqual('Intern');
 });
