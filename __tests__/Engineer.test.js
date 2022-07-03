b04954bdb4c4eeec001e05385e6df14e793d84fd
MosNes <7hekarl@gmail.com>
2022-07-03 12:25:27 -0400

    tested and added Engineer class and constructor

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,14 @@
+const Engineer = require ('../lib/Engineer');
+
+//test engineer class constructor
+test('create an Engineer Object with name, ID, email address, and github username', () => {
+    const engineer = new Engineer('bob','42','bob@company.com','DevBob');
+
+    console.log(engineer);
+
+    expect(engineer).toEqual(expect.any(Object));
+    expect(engineer.name).toEqual('bob');
+    expect(engineer.id).toEqual('42');
+    expect(engineer.email).toEqual('bob@company.com');
+    expect(engineer.github).toEqual('DevBob');
+});
